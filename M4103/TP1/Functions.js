0867804ce51ecd5b1d668994fc351089c6a45c2f
unknown <chloe.hetyei.2112@gmail.com>
2018-03-30 13:39:28 +0200

    pushtp1

---
+++ M4103/TP1/Functions.js
@@ -0,0 +1,8 @@
+alert("je suis dans le fichier Functions.js");
+function defTitre1(){
+    var notre_h1 = document.getElementById("titre");
+    notre_h1.innerText = "nouvelle valeur";
+    //console.log(notre_h1);
+    //alert "salut";
+}
+//uneAutreFonction();
