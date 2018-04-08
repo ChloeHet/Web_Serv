d073e53f3d4dd4d959221818790a8a18440b5633
unknown <chloe.hetyei.2112@gmail.com>
2018-04-08 13:33:18 +0200

    add end of tp1 (exercice7 et 8)

--- M4103/TPs/Functions.js
+++ M4103/TPs/Functions.js
@@ -60,3 +60,74 @@ function majNbJours(){
 
     jour = document.getElementById("nb_jours").innerHTML = "Il reste " + diffdate + " jours avant le 19 juillet 2018";
 }
+
+function majHorloge1(){
+    h = document.getElementById('horloge');
+    new_h = new Date();
+    h.innerHTML = new_h.getHours()+':'+new_h.getMinutes()+':'+new_h.getSeconds();
+}
+//window.setInterval('majHorloge1()',10000);
+
+function majHorloge2(){
+    h = document.getElementById('horloge');
+    new_h = new Date();
+    h.innerHTML = new_h.getHours()+':'+new_h.getMinutes()+':'+new_h.getSeconds();
+}
+//window.setTimeout('majHorloge2()',2000);
+
+function majGrafH() {
+    var currHour;
+    const node = document.getElementById("grafHorloge");
+    currHour = hour();
+    const chars = currHour.split('');
+
+    node.innerHTML = '';
+    for (const c in chars) {
+        if (chars[c] === ':') {
+            node.innerHTML += ':';
+        } else node.innerHTML = node.innerHTML
+            .concat('<img src="images/' + chars[c] + '.gif" />');
+    }
+}
+
+function nbInput(){
+    valueInput = document.getElementById("myInput").value;
+    if(isNaN(valueInput)) {
+        document.getElementById("myInput").className = "rouge";
+    }
+    else if (valueInput == "") {
+        document.getElementById("myInput").className = "blanc";
+    }
+    else {
+        document.getElementById("myInput").className = "vert";
+    }
+}
+
+function menuHoverPlus() {
+
+
+    document.getElementById("title").onmouseover = document.getElementById("PlusOuMoins").src = "http://users.polytech.unice.fr/~rey/images/minus.gif";
+    document.getElementById("title").onmouseout = document.getElementById("PlusOuMoins").src = "http://users.polytech.unice.fr/~rey/images/plus.gif";
+}
+
+sfHover = function() {
+    var sfEls = document.getElementById("nav").getElementsByTagName("li");
+    var img0 = document.getElementsByClassName("PlusOuMinus")[0];
+    var img1 = document.getElementsByClassName("PlusOuMinus")[1];
+    var img2 = document.getElementsByClassName("PlusOuMinus")[2];
+    for (var i=0; i<sfEls.length; i++) {
+        sfEls[i].onmouseover=function() {
+            this.className+=" sfhover";
+            img0.src = "http://users.polytech.unice.fr/~rey/images/minus.gif";
+            img1.src = "http://users.polytech.unice.fr/~rey/images/minus.gif";
+            img2.src = "http://users.polytech.unice.fr/~rey/images/minus.gif";
+        }
+        sfEls[i].onmouseout=function() {
+            this.classList.remove("sfhover");
+            img0.src = "http://users.polytech.unice.fr/~rey/images/plus.gif";
+            img1.src = "http://users.polytech.unice.fr/~rey/images/plus.gif";
+            img2.src = "http://users.polytech.unice.fr/~rey/images/plus.gif";
+        }
+    }
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
